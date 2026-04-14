e06d802bdf521d3b2691dcb423706cdbc8732c48
Lou Diamond Loyloy <122492118+loudiamond13@users.noreply.github.com>
2026-04-14 18:19:25 -0500

    Revert "Modified Server esproj to only reference frontend when in VS build #7"

--- CSPS.Domain/obj/Debug/net10.0/CSPS.Domain.AssemblyInfo.cs
+++ CSPS.Domain/obj/Debug/net10.0/CSPS.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSPS.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9759efdf31cef69e24f15b5e2d4c3376cb345be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSPS.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSPS.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
